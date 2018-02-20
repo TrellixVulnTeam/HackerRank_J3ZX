4a3567b0f1571d1cb73aa5efbf1686b5dce1faf8
FX196 <mikechen212@foxmail.com>
2018-02-20 10:31:20 -0800

    finished

--- Artificial Intelligence/Statistics and Machine Learning/Basic Statistics Warmup.py
+++ Artificial Intelligence/Statistics and Machine Learning/Basic Statistics Warmup.py
@@ -1,8 +1,16 @@
+from statistics import *
 n = input()
-x = input.split(' ')
-x = list(map(lambda s: int(s), x))
+x = input().split(" ")
+x = list(map(lambda s: float(s), x))
 mean = float(sum(x))/len(x)
-print(mean)
 x.sort()
-if n%2==0:
-    pass
+median = median(x)
+mode = mode(x)
+std = stdev(x)
+low = mean - 1.96*std
+hi = mean + 1.96*std
+print("%.1f" % mean)
+print("%.1f" % median)
+print("%d" % mode)
+print("%.1f" % std)
+print("%.1f %.1f" % (low, hi))
