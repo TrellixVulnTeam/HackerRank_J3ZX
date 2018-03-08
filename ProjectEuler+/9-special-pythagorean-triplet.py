2273dea121274a498d7c5102a09045296911635c
FX196 <mikechen212@foxmail.com>
2018-03-08 11:49:53 +0800

    style fixes

--- ProjectEuler+/9-special-pythagorean-triplet.py
+++ ProjectEuler+/9-special-pythagorean-triplet.py
@@ -4,11 +4,11 @@
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    max=-1
-    for a in range(n-1,0,-1):
-        b=(((2*a)-n)*n)/(2*(a-n))
-        c=n-a-b
-        if b==int(b) and c==int(c) and a!=b and b!=c and a!=c:
-            if a*b*c>max:
-                max=a*b*c
-    print(int(max))
+    maximum = -1
+    for a in range(n - 1, 0, -1):
+        b = (((2 * a) - n) * n) / (2 * (a - n))
+        c = n - a - b
+        if b == int(b) and c == int(c) and a != b and b != c and a != c:
+            if a * b * c > maximum:
+                maximum = a * b * c
+    print(int(maximum))
